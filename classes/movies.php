f6ee8f05275450a2b03893ae4a1cc67267b64d52
MembriniLuca29 <lucamembrini29@gmail.com>
2023-09-01 13:39:36 +0200

    primi punti fatti

---
+++ classes/movies.php
@@ -0,0 +1,14 @@
+<?php
+class movies {
+    public $title;
+    public $duration;
+    public $regia;
+
+    public function __construct($title, $duration, $regia, )
+    {
+        $this->title = $title;
+        $this->duration = $duration;
+        $this->regia = $regia;
+        
+    }
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__.'/classes/movies.php';
+
+$Oppenheimer =new movies('Oppenheimer',  '3h', 'Christopher Nolan');
+
+var_dump($Oppenheimer);
+
+$Barbie =new movies('Barbie',  '1h 54m', 'Greta Gerwig');
+
+var_dump($Barbie);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1
+</title>
+</head>
+<body>
+    
+</body>
+</html>
